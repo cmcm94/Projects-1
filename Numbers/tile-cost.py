bee013519bae5a70c3af8e1e0bb5d4a011e95e72
cmcm94 <33492247+cmcm94@users.noreply.github.com>
2017-12-01 08:10:33 -0500

    Create tile-cost.py

---
+++ Numbers/tile-cost.py
@@ -0,0 +1,4 @@
+cost=float(input('Cost of tile per sq foot? '))
+length=float(input('Length of the room, in feet? '))
+width=float(input('Width of the room, in feet? '))
+print(cost*length*width, 'is your total cost')
